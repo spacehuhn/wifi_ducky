6800e952dce32f3ccbf68eb5afdab91af03ed5c5
Stefan Kremser <stef.1497@gmail.com>
2017-03-14 21:45:26 +0100

    made formatBytes() more beautiful

--- esp8266_badUSB/esp8266_badUSB.ino
+++ esp8266_badUSB/esp8266_badUSB.ino
@@ -37,13 +37,13 @@ void viewScript(){
 //format bytes
 String formatBytes(size_t bytes){
   if (bytes < 1024){
-    return String(bytes)+"B";
+    return String(bytes)+" B";
   } else if(bytes < (1024 * 1024)){
-    return String(bytes/1024.0)+"KB";
+    return String(bytes/1024.0)+" KB";
   } else if(bytes < (1024 * 1024 * 1024)){
-    return String(bytes/1024.0/1024.0)+"MB";
+    return String(bytes/1024.0/1024.0)+" MB";
   } else {
-    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+    return String(bytes/1024.0/1024.0/1024.0)+" GB";
   }
 }
 
