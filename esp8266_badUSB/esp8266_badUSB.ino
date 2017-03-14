b86ca8cca95fee10982a345332f60c86cea53859
Stefan Kremser <spacehuhn@users.noreply.github.com>
2017-03-14 10:49:15 +0100

    working file upload

--- esp8266_badUSB/esp8266_badUSB.ino
+++ esp8266_badUSB/esp8266_badUSB.ino
@@ -13,6 +13,54 @@ extern char* data_getStyleCSS();
 extern char* data_getError404();
 extern char* data_getIndexHTML();
 extern char* data_getUploadHTML();
+//extern static char data_websiteBuffer[3000];
+
+void getScript(){
+  int i = 0;
+  for(int a=0;a<sizeof(data_websiteBuffer);a++) data_websiteBuffer[a] = 0;
+  if(server.hasArg("n")){
+    File f = SPIFFS.open(server.arg("n"), "r");
+    while(f.available()){
+      data_websiteBuffer[i] = f.read();
+      i++;
+    }
+  }
+  server.send(200, "text/plain", data_websiteBuffer);
+}
+
+//format bytes
+String formatBytes(size_t bytes){
+  if (bytes < 1024){
+    return String(bytes)+"B";
+  } else if(bytes < (1024 * 1024)){
+    return String(bytes/1024.0)+"KB";
+  } else if(bytes < (1024 * 1024 * 1024)){
+    return String(bytes/1024.0/1024.0)+"MB";
+  } else {
+    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+  }
+}
+
+void getScriptList() {
+  Dir dir = SPIFFS.openDir("");
+  String output;
+  output += "{";
+  output += "\"totalBytes\":"+(String)fs_info.totalBytes+",";
+  output += "\"usedBytes\":"+(String)fs_info.usedBytes+",";
+  output += "\"list\":[ ";
+  while(dir.next()){
+    File entry = dir.openFile("r");
+    String filename = String(entry.name()).substring(1);
+    output += '{';
+    output += "\"n\":\""+filename+"\",";  //name
+    output += "\"s\":\""+formatBytes(entry.size())+"\"";          //size
+    output += "},";
+    entry.close();
+  }
+  output = output.substring(0, output.length()-1);
+  output += "]}";
+  server.send(200, "text/json", output);
+}
 
 void setup() {
 
@@ -24,9 +72,9 @@ void setup() {
   Serial.begin(BAUD_RATE);
   delay(2000);
   Serial.println();
-  Serial.println();
 
   SPIFFS.begin();
+  //SPIFFS.format();
   SPIFFS.info(fs_info);
 
   server.onNotFound([](){ server.send(404, "text/html", data_getError404()); });
@@ -35,8 +83,8 @@ void setup() {
   server.on("/style.css",[](){ server.send(200, "text/css", data_getStyleCSS()); });
   server.on("/functions.js",[](){ server.send(200, "text/javascript", data_getFunctionsJS()); });
   server.on("/list.json", getScriptList);
+  server.on("/view", getScript);
   server.on("/",[](){ server.send(200, "text/html", data_getIndexHTML()); });
-  server.on("/index.html",[](){ server.send(200, "text/html", data_getIndexHTML()); });
 
   //handle upload
   server.on("/upload", HTTP_POST, [](){
@@ -44,53 +92,28 @@ void setup() {
     server.send ( 302, "text/plain", "");
   },[](){
     HTTPUpload& upload = server.upload();
-    File fsUploadFile;
-    
-    //starting upload
-    if(upload.status == UPLOAD_FILE_START){
-      Serial.println("uploading: "+upload.filename+"...");
-      fsUploadFile = SPIFFS.open(upload.filename, "w+");
-    }
-    
-    //while uploading
-    else if(upload.status == UPLOAD_FILE_WRITE){
+    if(upload.totalSize > 0){
+      File fsUploadFile;
+      
+      String filename = upload.filename;
+      if(!filename.startsWith("/")) filename = "/"+filename;
+        
+      Serial.println("uploading: "+filename+" ("+ (String)upload.totalSize+ "b)...");
+        
+      fsUploadFile = SPIFFS.open(filename, "a");
+      
       fsUploadFile.write(upload.buf, upload.currentSize);
-    } 
-
-    //finishing upload
-    else if(upload.status == UPLOAD_FILE_END){
+      //Serial.write(upload.buf, upload.currentSize);
+      
       fsUploadFile.close();
       Serial.println("upload done!");
+      yield();
     }
-     
-    yield();
-     
   });
     
   server.begin();
 
   Serial.println("started");
-}
-
-void getScriptList() {
-  Dir dir = SPIFFS.openDir("");
-  String output;
-  output += "{";
-  output += "\"totalBytes\":"+(String)fs_info.totalBytes+",";
-  output += "\"usedBytes\":"+(String)fs_info.usedBytes+",";
-  output = "\"list\":[";
-  while(dir.next()){
-    File entry = dir.openFile("r");
-    output += '{';
-    output += "\"n\":\""+(String)entry.name()+"\",";
-    output += "\"s\":"+(String)entry.size()+",";
-    output += "},";
-    entry.close();
-  }
-  output = output.substring(-1);
-  output += "]}";
-  server.send(200, "text/json", output);
-    
 }
   
 void loop() {
