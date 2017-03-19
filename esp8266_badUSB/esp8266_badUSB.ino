184747b0c616572f2161a1b3f3e154a77718e1e9
Stefan Kremser <stef.1497@gmail.com>
2017-03-19 22:18:36 +0100

    added OTA update

--- esp8266_badUSB/esp8266_badUSB.ino
+++ esp8266_badUSB/esp8266_badUSB.ino
@@ -22,6 +22,8 @@ bool runLine = false;
 bool runScript = false;
 File script;
 
+bool shouldReboot = false;
+
 const int bufferSize = 600;
 uint8_t scriptBuffer[bufferSize];
 uint8_t scriptLineBuffer[bufferSize];
@@ -159,6 +161,37 @@ void setup() {
   server.onNotFound([](AsyncWebServerRequest *request){
     send404(request);
   });
+
+  // Simple Firmware Update Form
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>");
+  });
+  server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request){
+    shouldReboot = !Update.hasError();
+    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", shouldReboot?"OK":"FAIL");
+    response->addHeader("Connection", "close");
+    request->send(response);
+  },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
+    if(!index){
+      Serial.printf("Update Start: %s\n", filename.c_str());
+      Update.runAsync(true);
+      if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
+        Update.printError(Serial);
+      }
+    }
+    if(!Update.hasError()){
+      if(Update.write(data, len) != len){
+        Update.printError(Serial);
+      }
+    }
+    if(final){
+      if(Update.end(true)){
+        Serial.printf("Update Success: %uB\n", index+len);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
   
 
   server.begin();
@@ -182,6 +215,8 @@ void addToBuffer(){
 }
 
 void loop() {
+  if(shouldReboot) ESP.restart();
+  
   if(Serial.available() > 0) {
     byte answer = Serial.read();
     if(answer == 0x99) {
